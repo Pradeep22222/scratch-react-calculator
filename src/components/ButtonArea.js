247815bcfe1b4d8027c341c095258b5217428f08
PK <pradeepdhital124@gmail.com>
2022-12-23 07:58:22 +1100

    basic layout is done

---
+++ src/components/ButtonArea.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const ButtonArea = () => {
+  return (
+    <>
+      <div class="box box-AC">AC</div>
+      <div class="box pdm">C</div>
+      <div class="box box-percent">%</div>
+      <div class="box box-divide">/</div>
+      <div class="box box-7">7</div>
+      <div class="box box-8">8</div>
+      <div class="box box-9">9</div>
+      <div class="box box-x">*</div>
+      <div class="box box-4">4</div>
+      <div class="box box-5">5</div>
+      <div class="box box-6">6</div>
+      <div class="box box-minus">-</div>
+      <div class="box box-1">1</div>
+      <div class="box box-2">2</div>
+      <div class="box box-3">3</div>
+      <div class="box box-plus">+</div>
+      <div class="box box-zero">0</div>
+      <div class="box box-dot">.</div>
+      <div class="box box-equals">=</div>
+    </>
+  );
+}
